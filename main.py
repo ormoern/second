90aa6081fba49dd817d86a183826abf553742280
ormoern <ilja.soroko@gmail.com>
2026-03-23 22:23:48 +0200

    123

--- main.py
+++ main.py
@@ -66,26 +66,31 @@ def check_time_format(time):
 def handle_input(drink, time, cust_caff_amnt):
     print("start check")
     try:
-        if cust_caff_amnt == "" and drink:
-            st.info('No caffeine amount, yet there is a drink...', icon="🤔")
-        if cust_caff_amnt and drink == "":
-            st.info('No custom drink, yet there is a caffeine amount...', icon="🤔")
-        if (drink or cust_caff_amnt) and time == "":
-            st.info('No time...', icon="🕙")
-            return None
-        elif not check_time_format(time):
-            st.info('Check time format (ex: 12.30)', icon="🕙")
-            return None
-        elif not re.findall(r"^\dt$", cust_caff_amnt):
-            st.info("Only numbers are allowed.")
-        elif int(cust_caff_amnt) <= 1 or int(cust_caff_amnt) >= 1000:
-            st.info("Invalid number.")
-        elif drink and check_time_format(time):
-            if cust_caff_amnt:
-                est_caffeine = int(cust_caff_amnt)
-                return time, drink, est_caffeine
-            else:
+        if drink in caffeine_amount:
+            if (drink or cust_caff_amnt) and time == "":
+                st.info('No time...', icon="🕙")
+                return None
+            elif not check_time_format(time):
+                st.info('Check time format (ex: 12.30)', icon="🕙")
+                return None
+            else drink and check_time_format(time)::
                 est_caffeine = caffeine_amount[drink]
+                return time, drink, est_caffeine
+        elif drink not in caffeine_amount:
+            if cust_caff_amnt == "" and drink:
+                st.info('No caffeine amount, yet there is a drink...', icon="🤔")
+                return None
+            if cust_caff_amnt and drink == "":
+                st.info('No custom drink, yet there is a caffeine amount...', icon="🤔")
+                return None
+            elif not re.findall(r"^\dt$", cust_caff_amnt):
+                st.info("Only numbers are allowed.")
+                return None
+            elif int(cust_caff_amnt) <= 1 or int(cust_caff_amnt) >= 1000:
+                st.info("Invalid number.")
+                return None
+            else drink and check_time_format(time):
+                est_caffeine = int(cust_caff_amnt)
                 return time, drink, est_caffeine
         else:
             st.info('Uhh, what?')
