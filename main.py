ef3332805d79073380ea142c4288660b16436f5b
ormoern <ilja.soroko@gmail.com>
2026-03-23 20:02:26 +0200

    custom drink rework

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def handle_input(drink, time, cust_caff_amnt):
             return None
         elif re.findall("^\d*$", cust_caff_amnt) is None:
             st.info("Only numbers are allowed.")
-        elif if 0 < int(cust_caff_amnt) <= 1000:
+        elif 0 < int(cust_caff_amnt) <= 1000:
             st.info("Invalid number.")
         elif drink and check_time_format(time):
             if cust_caff_amnt:
