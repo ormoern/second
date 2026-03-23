e5e1af08c01f441ae325b48b938d2b29b7d24b74
ormoern <ilja.soroko@gmail.com>
2026-03-23 20:07:53 +0200

    rework

--- main.py
+++ main.py
@@ -105,12 +105,11 @@ def add_data(time, drink, est_caffeine):
     return
 
 if st.button("Add"):
-    if custom_drink != "" and custom_caff != "":
-        time, drink, est_caffeine = handle_input(custom_drink, time, custom_caff)
+    if custom_drink == "" and custom_caff == "":
+        time, drink, est_caffeine = handle_input(drink, time)
         add_data(time, drink, est_caffeine)
     else: 
-        handle_input(drink, time)
-        time, drink, est_caffeine = handle_input(drink, time)
+        time, drink, est_caffeine = handle_input(custom_drink, time, custom_caff)
         add_data(time, drink, est_caffeine)
 
     
